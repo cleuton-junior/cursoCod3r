85d3c1da5e41bd33c461363065c794c35d4141c6
Cleuton Barbosa Agripino Junior <ext-cleuton.agripino@bbts.com.br>
2020-12-07 10:26:48 -0300

    implementação de cadastro de produtos

---
+++ src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,48 @@
+import { HeaderService } from './../../template/header/header.service';
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  propLegal= "qualquer"
+
+  product: Product = {
+      name: '',
+      price: null
+  }
+
+  constructor(private productService: ProductService,
+    private router: Router, private headerService: HeaderService) {
+      headerService.headerData = {
+        title: '+ Cadastro de Produtos',
+        icon: 'storefront',
+        routeUrl: '/products/create'
+      }
+     }
+
+  ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+    
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+  fazerAlgo(): void {
+    console.log("Fazendo algo!")
+  }
+}
